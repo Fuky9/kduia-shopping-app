2d79dce366db6372eb6dc2eb4076d9f55dbf9f55
Fuky9 <lukasfukan6@gmail.com>
2024-05-16 22:24:54 +0200

    Finished code for three components: CartValue, ExpenseList, ExpenseItem

--- src/components/ItemSelected.js
+++ src/components/ItemSelected.js
@@ -1,7 +1,6 @@
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
-const ItemSelected = (props) => {
-};
+const ItemSelected = (props) => {};
 
 export default ItemSelected;
